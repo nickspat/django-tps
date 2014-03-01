61361c14f9d644fd4f45dfd5d35d317a4eb18aa9
Christopher Grant <crgrant@gmail.com>
2014-03-01 11:35:09 -0600

    initial commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+import os
+import tps
+
+
+setup(
+    author='Christopher Grant',
+    author_email='crgrant@gmail.com',
+    name='django-tps',
+    version='0.1',
+    description='Django based TPS systm',
+    url='',
+    install_requires=[  ],
+    packages=find_packages(),
+
+)
---
+++ status_app/tests/test.py
@@ -0,0 +1,8 @@
+import nose.tools as nt
+
+class TestSample(object):
+
+
+    def test_sample(self):
+	assert True
+
